29ea4148236b4401bc56acc2a71d94b31621f8ff
qin-gs <3210968849@qq.com>
2021-08-07 16:21:41 +0800

    初始化

---
+++ apache-code/src/main/java/com/apache/pojo/User.java
@@ -0,0 +1,82 @@
+package com.apache.pojo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class User {
+	private String name;
+	private int age;
+	private String address;
+	private List<String> list;
+	private Map<String, Object> map;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		User user = (User) o;
+		return age == user.age && Objects.equals(name, user.name) && Objects.equals(address, user.address) && Objects.equals(list, user.list) && Objects.equals(map, user.map);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, age, address, list, map);
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"name='" + name + '\'' +
+				", age=" + age +
+				", address='" + address + '\'' +
+				", list=" + list +
+				", map=" + map +
+				'}';
+	}
+
+	public Map<String, Object> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<String, Object> map) {
+		this.map = map;
+	}
+
+	public List<String> getList() {
+		return list;
+	}
+
+	public void setList(List<String> list) {
+		this.list = list;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
